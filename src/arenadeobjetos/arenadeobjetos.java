f0b401c190c91ea315d1dc5d452bd8d5c0e48140
JosmarFalcade <josmarfalcade1996@gmail.com>
2024-09-04 09:44:57 -0300

    JogoArena1

---
+++ src/arenadeobjetos/arenadeobjetos.java
@@ -0,0 +1,26 @@
+package arenadeobjetos;
+
+import model.Guerreiro;
+import model.Pessoa;
+
+public class arenadeobjetos {
+    public static void main(String[] args) {
+
+        Pessoa vitima = new Pessoa("Vitima 1", "Castanho", "Castanho Claro", "Branca", true);
+        System.out.println("Vitima:\n" + vitima.toString());
+
+        System.out.println("Perdeu 25 pontos de vida");
+        int pvAtual = vitima.getPontosDeVida();
+        vitima.setPontosDeVida(pvAtual - 25);
+        System.out.println("Vitima:\n" + vitima.toString());
+
+        System.out.println("Perdeu 30 pontos de vida");
+        vitima.dano(30);
+        System.out.println("Vitima:\n" + vitima.toString());
+
+        Guerreiro denis = new Guerreiro("Denis", "Cinza", "Verde", "Branca", false, "Espada Longa");
+        System.out.println("Guerreiro:\n" + denis.toString());
+        System.out.println("123");
+    }
+
+}
---
+++ src/model/Guerreiro.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Guerreiro extends Pessoa {
+    private String armamento;
+
+    public Guerreiro() {
+    }
+
+    public Guerreiro(String nome, String cabelo, String olho, String pele, boolean sexo, String armamento) {
+        super(nome, cabelo, olho, pele, sexo);
+        this.armamento = armamento;
+    }
+
+    public String getArmamento() {
+        return armamento;
+    }
+
+    public void setArmamento(String armamento) {
+        this.armamento = armamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Guerreiro [armamento=" + armamento + ", getArmamento()=" + getArmamento() + ", getNome()=" + getNome()
+                + ", getCabelo()=" + getCabelo() + ", getOlho()=" + getOlho() + ", getPele()=" + getPele()
+                + ", isSexo()=" + isSexo() + ", getPontosDeVida()=" + getPontosDeVida() + "]";
+    }
+
+}
