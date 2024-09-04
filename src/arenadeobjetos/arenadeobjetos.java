5474658168fa498d343be1836816d6b8d43462c2
JosmarFalcade <josmarfalcade1996@gmail.com>
2024-09-04 12:00:01 -0300

    aula2

--- src/arenadeobjetos/arenadeobjetos.java
+++ src/arenadeobjetos/arenadeobjetos.java
@@ -1,26 +1,70 @@
 package arenadeobjetos;
 
-import model.Guerreiro;
-import model.Pessoa;
+import java.util.Scanner;
 
 public class arenadeobjetos {
-    public static void main(String[] args) {
+    static Scanner ler = new Scanner(System.in);
 
-        Pessoa vitima = new Pessoa("Vitima 1", "Castanho", "Castanho Claro", "Branca", true);
-        System.out.println("Vitima:\n" + vitima.toString());
-
-        System.out.println("Perdeu 25 pontos de vida");
-        int pvAtual = vitima.getPontosDeVida();
-        vitima.setPontosDeVida(pvAtual - 25);
-        System.out.println("Vitima:\n" + vitima.toString());
-
-        System.out.println("Perdeu 30 pontos de vida");
-        vitima.dano(30);
-        System.out.println("Vitima:\n" + vitima.toString());
-
-        Guerreiro denis = new Guerreiro("Denis", "Cinza", "Verde", "Branca", false, "Espada Longa");
-        System.out.println("Guerreiro:\n" + denis.toString());
-        System.out.println("123");
+    public static int lerInt() {
+        Scanner ler = new Scanner(System.in);
+        int num = 99;
+        try {
+            num = ler.nextInt();
+        } catch (Exception e) {
+            System.out.print("Tente novamente: ");
+            lerInt();
+        }
+        return num;
     }
 
+    public static void main(String[] args) {
+        /*
+         * Pessoa vitima = new Pessoa("Vitima 1", "Castanho", "Castanho Claro",
+         * "Branca", true);
+         * System.out.println("Vitima:\n" + vitima.toString());
+         * System.out.println("Perdeu 25 pontos de vida");
+         * int pvAtual = vitima.getPontosDeVida();
+         * vitima.setPontosDeVida(pvAtual - 25);
+         * System.out.println("Vitima:\n" + vitima.toString());
+         * System.out.println("Perdeu 30 pontos de vida");
+         * vitima.dano(30);
+         * System.out.println("Vitima:\n" + vitima.toString());
+         * Guerreiro denis = new Guerreiro("Denis", "Cinza", "Verde", "Branca", false,
+         * "Espada Longa");
+         * System.out.println("Guerreiro:\n" + denis.toString());
+         */
+        /*
+         * String texto;
+         * int num;
+         * System.out.print("Digite seu nome: ");
+         * texto = ler.nextLine();
+         * System.out.print("Digite sua idade:");
+         * num = lerInt();
+         */
+    }
+
+    public static void menuP() {
+        String menu;
+        menu = "1 - Criar Personagens"
+                + "\n2 - Listar Personagens"
+                + "\n3 - Batalha"
+                + "\n0 - Sair do Jogo";
+        System.out.println(menu);
+    }
+
+    public static void subMenuP(int omP) {
+        String subMenu;
+        switch (omP) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
 }
--- src/model/Ladrao.java
+++ src/model/Ladrao.java
@@ -1,5 +1,34 @@
 package model;
 
 public class Ladrao extends Pessoa {
-    
+    private String planoDeFuga;
+
+    public Ladrao() {
+
+    }
+
+    public Ladrao(String nome, String cabelo, String olho, String pele, boolean sexo, String planoDeFuga) {
+        super(nome, cabelo, olho, pele, sexo);
+        this.planoDeFuga = planoDeFuga;
+    }
+
+    public String getPlanoDeFuga() {
+        return planoDeFuga;
+    }
+
+    public String fugir() {
+        return "Dar no pé!";
+    }
+
+    public String roubar() {
+        return "Arrastão";
+    }
+
+    @Override
+    public String toString() {
+        return "Ladrao [getPlanoDeFuga()=" + getPlanoDeFuga() + ", getNome()=" + getNome() + ", getCabelo()="
+                + getCabelo() + ", getOlho()=" + getOlho() + ", getPele()=" + getPele() + ", isSexo()=" + isSexo()
+                + ", getPontosDeVida()=" + getPontosDeVida() + "]";
+    }
+
 }
